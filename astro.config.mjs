ed002d397aa78feba4c8240dd1c0a949bd23e700
Masumi Kawasaki <geeknees@gmail.com>
2024-02-17 19:18:31 +0900

    Add GitHub Pages deployment workflow and update Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://geeknees.github.io',
+  base: '/astrokyo',
   integrations: [tailwind()]
-});
+})
